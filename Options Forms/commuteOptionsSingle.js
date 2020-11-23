4c394f1355a9d7bf2c40ff0acad01c72694d2b7e
Ethan <46939195+Ferris-Bueller7@users.noreply.github.com>
2020-11-23 00:57:55 -0800

    cont. work on moving the data
    Not sure if it works yet or not.

--- Options Forms/commuteOptionsSingle.js
+++ Options Forms/commuteOptionsSingle.js
@@ -1,58 +1,67 @@
+$(document).ready(function(){
+  (function() {
+    'use strict';
+    window.addEventListener('load', function() {
+      // Fetch all the forms we want to apply custom Bootstrap validation styles to
+      var forms = document.getElementsByClassName('needs-validation');
+      // Loop over them and prevent submission
+      var validation = Array.prototype.filter.call(forms, function(form) {
+          form.addEventListener('submit', function(event) {
+          if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+          }
+          form.classList.add('was-validated');
+        }, false);
+      });
+    }, false);
+  })();
+});
+
+
+  // $('#form').submit(function(e){
+  //     e.preventDefault();
+  //     let userData = { 
+  //         mpg: $('input#mpg').val(),
+  //         //address: $('input#address').val(),
+  //         insurance: $('input#insurance').val(),
+  //         parking: $('input#park').val()
+  //     };
+  //     window.localStorage.setItem('userData', JSON.stringify(userData));
+  //     console.log(window.localStorage.getItem('userData'));
+  // });
+  // $('#submit').click(function(){
+  //     window.location.replace('output.html');
+  // });
+
+// const wrapper = document.querySelector('.wrapper'),
+//       form = wrapper.querySelectorAll('.form'),
+//       submitInput = form[0].querySelector('input[type = "submit"]');
+
+
+
+
 // $(document).ready(function(){
-//     $('#customControlValidation').click(function(){
-//         $('#customControlValidation').toggle();
-
-//     })
-
-// })
-
-// $('#submit').click(function(){
-//     window.location.href = '../Data Analysis.html';
+//   $('#form').submit(function(e){
+//       e.preventDefault();
+//       let userData = { 
+//           mpg: $('input#mpg').val(),
+//           address: $('input#address').val(),
+//           insurance: $('input#insurance').val(),
+//           parking: $('input#park').val()
+//       };
+//       window.localStorage.setItem('userData', JSON.stringify(userData));
+//       console.log(window.localStorage.getItem('userData'));
+//   });
+//   $('#submit').click(function(){
+//       window.location.replace('output.html');
+//   });
 // });
-
-// Example starter JavaScript for disabling form submissions if there are invalid fields
-(function() {
-  'use strict';
-  window.addEventListener('load', function() {
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    var forms = document.getElementsByClassName('needs-validation');
-    // Loop over them and prevent submission
-    var validation = Array.prototype.filter.call(forms, function(form) {
-        form.addEventListener('submit', function(event) {
-        if (form.checkValidity() === false) {
-          event.preventDefault();
-          event.stopPropagation();
-        }
-        form.classList.add('was-validated');
-      }, false);
-    });
-  }, false);
-})();
-
-// document.getElementById("sub").onclick = function () {
-//     location.href = "../Data Analysis.html";
-// };
-// $('submit#sub').submit(function() {
-//     window.location.href = "../Data Analysis.html"
-// })
-
-// let userData = { 
-//     mpg: $('input#mpg').val(),
-//     address: $('input#address').val(),
-//     insurance: $('input#insurance').val(),
-//     parking: $('input#park').val()
-// };
-// window.localStorage.setItem('userData', JSON.stringify(userData));
-
-// let temp = document.querySelector('#mpg').value
-// console.log(temp);
-// let form = document.querySelectorAll('.needs-validation')
-// {
-//     form.addEventListener('submit',function(event){
-//         if(form.checkValididty()===false){
-//             event.preventDefault();
-//             event.stopPropagation();
-//         }
-//         form.classList.add('was-validated');
-//     })
-// }
+function greetings() {
+  var milesPerGallon = document.getElementById("mpg").value;
+  var insuranceCost = document.getElementById("insuranceCost").value;
+  // var milesPerGallon = document.getElementById("mpg").value;
+  console.log(milesPerGallon);
+  console.log(insuranceCost);
+  //document.write("Hello" + milesPerGallon +insuranceCost);
+}
