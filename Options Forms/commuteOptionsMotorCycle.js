d42bf0b8c0094c6c920e88dd1babe0c167a2df5a
Ethan <46939195+Ferris-Bueller7@users.noreply.github.com>
2020-11-23 20:55:03 -0800

    Update commuteOptionsMotorCycle.js

--- Options Forms/commuteOptionsMotorCycle.js
+++ Options Forms/commuteOptionsMotorCycle.js
@@ -22,12 +22,12 @@
     var mode = 'Motorcycle';
     var milesPerGallon = document.getElementById("mpg").value;
     var insuranceCost = document.getElementById("insuranceCost").value;
-    var address = document.getElementById("addy").value;
+    // var address = document.getElementById("addy").value;
     window.localStorage.setItem('mpg', milesPerGallon);
     window.localStorage.setItem('insurance', insuranceCost);
-    window.localStorage.setItem('addy', address);
+    // window.localStorage.setItem('addy', address);
     window.localStorage.setItem('mode',mode);
-    console.log(milesPerGallon);
-    console.log(insuranceCost);
+    // console.log(milesPerGallon);
+    // console.log(insuranceCost);
     //document.write("Hello" + milesPerGallon +insuranceCost);
   }
