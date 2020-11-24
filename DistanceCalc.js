88eb26e0bbece0e2f0843dd524b5752b3e4c6a58
DLeineke <daleineke@yahoo.com>
2020-11-23 20:54:04 -0800

    Maps and Stuff
    Implemented maps data transfer and input

--- DistanceCalc.js
+++ DistanceCalc.js
@@ -24,9 +24,17 @@ var directionsDisplay = new google.maps.DirectionsRenderer();
 // Bind the DirectionsRenderer to the map
 directionsDisplay.setMap(map);
 
+var dataStore = [
+    [0, 0],         // DRIVE
+    [0, 0],         // WALK
+    [0, 0],         // BIKE
+    [0, 0]          // TRANSIT
+];
+
+let userType = window.localStorage.getItem('mode');
 
 // Define calcRoute function
-function calcRoute(theMode) {
+function calcRoute(theMode, count) {
     //create request
     var request = {
         origin: document.getElementById("addy").value,
@@ -36,31 +44,61 @@ function calcRoute(theMode) {
         unitSystem: google.maps.UnitSystem.IMPERIAL
     }
 
+
     // Routing
     directionsService.route(request, function (result, status) {
         if (status == google.maps.DirectionsStatus.OK) {
 
             //Get distance and time
             distance = result.routes[0].legs[0].distance;
-            duration = result.routes[0].legs[0].duration;             
-            $("#output").html("<div class='result-table'> Driving distance: " + result.routes[0].legs[0].distance.text + ".<br />Duration: " + result.routes[0].legs[0].duration.text + ".</div>");
-            document.getElementById("output").style.display = "block";
-
-            //display route
-            directionsDisplay.setDirections(result);
+            duration = result.routes[0].legs[0].duration;
+            dataStore[count][0] = distance.text;
+            dataStore[count][1] = duration.text;
+            // $("#output").html("<div class='result-table'> Driving distance: " + result.routes[0].legs[0].distance.text + ".<br />Duration: " + result.routes[0].legs[0].duration.text + ".</div>");
+            // document.getElementById("output").style.display = "block";
+            if (count == userType)
+                //display route
+                directionsDisplay.setDirections(result);
         } else {
             //delete route from map
             directionsDisplay.setDirections({ routes: [] });
             //center map in London
             map.setCenter(sacState);
 
-            //Show error message           
+            //Show error message 
            
             alert("Can't find road! Please try again!");
             clearRoute();
         }
     });
 
+}
+
+function processType() {
+    if (userType == 'SOV')
+        userType = 0;
+    else if (userType == 'MOV')
+        userType = 0;
+    else if (userType == 'Motorcycle')
+        userType = 0;
+    else if (userType == 'Transit')
+        userType = 3;
+    else if (userType == 'Bike')
+        userType = 2;
+    else
+        userType = 1;
+}
+
+function calcAll() {
+    console.log(userType);
+    processType();
+    console.log(userType);
+    calcRoute(google.maps.TravelMode.DRIVING, 0);
+    calcRoute(google.maps.TravelMode.WALKING, 1);
+    calcRoute(google.maps.TravelMode.BICYCLING, 2);
+    calcRoute(google.maps.TravelMode.TRANSIT, 3);
+    console.log(dataStore);
+    window.localStorage.setItem('travelInfo', dataStore);
 }
 
 // Clear results
