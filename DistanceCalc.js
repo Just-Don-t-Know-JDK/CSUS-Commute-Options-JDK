a8563f053329e6681a92832e67c8d95571c14185
JoseUTorres <73760867+JoseUTorres@users.noreply.github.com>
2020-11-23 18:41:07 -0800

    refinements

--- DistanceCalc.js
+++ DistanceCalc.js
@@ -1,7 +1,7 @@
 // Functions for calculating distance between sacstate and their address
 // Sacstate coords
-var sacState = { lat: 38.56122, lng: -121.42426 };
-var mapOptions = {
+const sacState = { lat: 38.56122, lng: -121.42426 };
+const mapOptions = {
     center: sacState,
     zoom: 13,
     mapTypeId: google.maps.MapTypeId.ROADMAP
@@ -13,7 +13,7 @@ let distance, duration;
 document.getElementById("output").style.display = "none";
 
 // Create/Init map
-var map = new google.maps.Map(document.getElementById('google-map'), mapOptions);
+const map = new google.maps.Map(document.getElementById("google-map"), mapOptions);
 
 // Create a DirectionsService object to use the route method and get a result for our request
 var directionsService = new google.maps.DirectionsService();
@@ -29,7 +29,7 @@ directionsDisplay.setMap(map);
 function calcRoute() {
     //create request
     var request = {
-        origin: document.getElementById("location-1").value,
+        origin: document.getElementById("addy").value,
         destination: sacState,
         travelMode: google.maps.TravelMode.DRIVING,
         unitSystem: google.maps.UnitSystem.IMPERIAL
@@ -66,8 +66,7 @@ function calcRoute() {
 
 function clearRoute(){
     document.getElementById("output").style.display = "none";
-    document.getElementById("location-1").value = "";
-    document.getElementById("location-2").value = "";
+    document.getElementById("addy").value = "";
     directionsDisplay.setDirections({ routes: [] });
     
 }
@@ -79,8 +78,5 @@ var options = {
 }
 
 
-var input1 = document.getElementById("location-1");
+var input1 = document.getElementById("addy");
 var autocomplete1 = new google.maps.places.Autocomplete(input1, options);
-
-var input2 = document.getElementById("location-2");
-var autocomplete2 = new google.maps.places.Autocomplete(input2, options);
