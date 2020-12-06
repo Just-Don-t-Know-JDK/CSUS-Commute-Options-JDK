131d78aa1c50a6fec900baafbf9a475c0bff8fd6
DLeineke <daleineke@yahoo.com>
2020-12-05 21:02:58 -0800

    Databases
    Added some database functionality just for examples

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const { response } = require('express');
 const express = require('express');
+const Datastore = require('nedb');
 const app = express();
 app.listen(3000, () => console.log('listening at 3000'));
 
@@ -7,9 +8,21 @@ app.listen(3000, () => console.log('listening at 3000'));
 app.use(express.static('public'));
 app.use(express.json({ limit: '1mb' }));
 
-// NEED THE SURVEY & THE Data Analysis INFO, ALSO POST API KEY
+// Init the commute info db and survey db
+const commuteInfoDB = new Datastore('commuteInfo.db');
+const surveyDB = new Datastore('survey.db');
+commuteInfoDB.loadDatabase();
+surveyDB.loadDatabase();
+
+// receive survey data for survey.db
+app.post('/survey', (request, response) => {
+    // stuff goes here for the survey responses...
+});
+
+// receive the commute info data for commuteInfo.db
 app.post('/userData', (request, response) => {
     console.log(request.body);
+    commuteInfoDB.insert(request.body);
     response.json({
         status: 'success'
     });
